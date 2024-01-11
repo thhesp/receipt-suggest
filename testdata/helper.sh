54df43a280211c3bebf483fb261c2a65e31fffe1
thomas_s <github@sproed.de>
2024-01-12 00:12:46 +0100

    Improved helper file to create empty files

--- testdata/helper.sh
+++ testdata/helper.sh
@@ -1,3 +1,5 @@
 # Helper script to create folders for each receipe
 # has to be run from data or testdata folder
-tail -n +2 receipes.csv | cut -d ',' -f2 | xargs -i{} mkdir -p "receipe/{}"
+tail -n +2 recipes.csv | cut -d ',' -f2 | xargs -i{} mkdir -p "receipe/{}"
+ls -d recipe/* | xargs -I {} bash -c "cd '{}' && touch ingredients.csv"
+ls -d recipe/* | xargs -I {} bash -c "cd '{}' && touch description.txt"
