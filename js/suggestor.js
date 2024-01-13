36d3a7e1b430b4a77e015cd9f28e30bc819ba6be
Thomas <github@sproed.de>
2024-01-13 22:36:51 +0100

    Added filtering and external recipes

--- js/suggestor.js
+++ js/suggestor.js
@@ -24,17 +24,44 @@ function onGenerateSuggestions(){
 
 function getSuggestions(csv){
 	var receipts = jQuery.csv.toObjects(csv);
-	console.log(receipts);
+	console.log("All ", receipts);
 
-	const shuffled = receipts.sort(() => 0.5 - Math.random());
+	// remove all which shouldn't be included
+	receipts = receipts.filter(recipe => recipe.Include === 'Y');
+
+	console.log("Filtered ", receipts);
+
+	// shuffle the entries around
+	shuffleSuggestions(receipts);
 
 	// Get sub-array of first n elements after shuffled
-	return shuffled.slice(0, 5);
+	return receipts.slice(0, 5);
 }
 
 function transformData(data){
 	return {
 		name:data['Name'], 
-		link: "recipe.html?recipe="+data['Link']+"&name="+data['Name']
+		link: getLink(data)
 	};
+}
+
+function getLink(data){
+	if(data.External === 'Y'){
+		return data['Link'];
+	}
+
+	return "recipe.html?recipe="+data['Link']+"&name="+data['Name'];
+}
+
+function shuffleSuggestions(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1)); // random index from 0 to i
+
+    // swap elements array[i] and array[j]
+    // we use "destructuring assignment" syntax to achieve that
+    // you'll find more details about that syntax in later chapters
+    // same can be written as:
+    // let t = array[i]; array[i] = array[j]; array[j] = t
+    [array[i], array[j]] = [array[j], array[i]];
+  }
 }
