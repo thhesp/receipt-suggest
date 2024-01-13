92d2f273794162d1be24b8b9a14e50346c2d9bd4
Thomas <github@sproed.de>
2024-01-13 17:07:57 +0100

    Adapted ps1 script to build base image only

--- build.ps1
+++ build.ps1
@@ -1,4 +1,4 @@
 docker stop receipt-suggest-test
 docker rm receipt-suggest-test
-Get-Content Dockerfile | docker build -t receipt-suggest:v1 .
+Get-Content Dockerfile | docker build --target base -t receipt-suggest:v1 .
 docker run --name receipt-suggest-test -d -p 8088:80 receipt-suggest:v1
